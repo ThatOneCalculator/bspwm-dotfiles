ea77b716b84ed47d4e11ca99b26e03b372ea2d40
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2021-03-25 11:08:10 -0700

    Delete vscode-oss directory

--- vscode-oss/extensions/lukas-tr.materialdesignicons-intellisense-3.1.0/out/requests.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=requests.js.map
--- vscode-oss/extensions/ms-python.python-2021.3.658691958/pythonFiles/lib/python/jedi_language_server/constants.py
+++
@@ -1,4 +0,0 @@
-"""Constants."""
-
-# From `jedi.inference.references._IGNORE_FOLDERS`. Prevents using private var
-JEDI_IGNORE_FOLDERS = {".tox", ".venv", "venv", "__pycache__"}
